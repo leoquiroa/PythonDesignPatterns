80f79bfed6e5b6b35d0436a4c1b9ee1e2b19cd48
leoquiroa <leonelquiroa@gmail.com>
2021-01-01 11:27:03 -0600

    Abstract Factory with 4 classes

--- scripts/Factory.py
+++ scripts/Factory.py
@@ -1,28 +1,3 @@
-"""
-*What is this pattern about?
-In Java and other languages, the Abstract Factory Pattern serves to provide an interface for
-creating related/dependent objects without need to specify their
-actual class.
-The idea is to abstract the creation of objects depending on business
-logic, platform choice, etc.
-In Python, the interface we use is simply a callable, which is "builtin" interface
-in Python, and in normal circumstances we can simply use the class itself as
-that callable, because classes are first class objects in Python.
-*What does this example do?
-This particular implementation abstracts the creation of a pet and
-does so depending on the factory we chose (Dog or Cat, or random_animal)
-This works because both Dog/Cat and random_animal respect a common
-interface (callable for creation and .speak()).
-Now my application can create pets abstractly and decide later,
-based on my own criteria, dogs over cats.
-*Where is the pattern used practically?
-*References:
-https://sourcemaking.com/design_patterns/abstract_factory
-http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
-*TL;DR
-Provides a way to encapsulate a group of individual factories.
-"""
-
 import random
 
 
@@ -41,59 +16,64 @@ class PetShop:
         pet = self.pet_factory()
         print("We have a lovely {}".format(pet))
         print("It says {}".format(pet.speak()))
+        print("walks on {} legs".format(pet.legs()))
 
 
 class Dog:
-    def speak(self):
-        return "woof"
-
-    def __str__(self):
-        return "Dog"
-
+    def __str__(self): return "Dog"
+    def speak(self): return "woof"
+    def legs(self): return 4
 
 class Cat:
-    def speak(self):
-        return "meow"
-
-    def __str__(self):
-        return "Cat"
-
-
+    def __str__(self): return "Cat"
+    def speak(self): return "meow"
+    def legs(self): return 4
+    
 # Additional factories:
+
+class Parrot:
+    def __str__(self): return "Parrot"
+    def speak(self): return "fiu fiu"
+    def legs(self): return 2
+
+class Monkey:
+    def __str__(self): return "Monkey"
+    def speak(self): return "uh ah"
+    def legs(self): return 2
+    
 
 # Create a random animal
 def random_animal():
     """Let's be dynamic!"""
-    return random.choice([Dog, Cat])()
+    possible = [Dog, Cat, Parrot, Monkey]
+    return random.choice(possible)()
 
 
 # Show pets with various factories
 def main():
-    """
+    
     # A Shop that sells only cats
-    >>> cat_shop = PetShop(Cat)
-    >>> cat_shop.show_pet()
-    We have a lovely Cat
-    It says meow
+    cat_shop = PetShop(Cat)
+    cat_shop.show_pet()
+    #We have a lovely Cat
+    #It says meow
     # A shop that sells random animals
-    >>> shop = PetShop(random_animal)
-    >>> for i in range(3):
-    ...    shop.show_pet()
-    ...    print("=" * 20)
-    We have a lovely Cat
-    It says meow
-    ====================
-    We have a lovely Dog
-    It says woof
-    ====================
-    We have a lovely Dog
-    It says woof
-    ====================
-    """
-
+    shop = PetShop(random_animal)
+    for i in range(10):
+        shop.show_pet()
+        print("--")
+    #We have a lovely Cat
+    #It says meow
+    #====================
+    #We have a lovely Dog
+    #It says woof
+    #====================
+    #We have a lovely Dog
+    #It says woof
+    #====================
 
 if __name__ == "__main__":
-    random.seed(1234)  # for deterministic doctest outputs
-    import doctest
-
-    doctest.testmod()
+    random.seed(42)  # for deterministic doctest outputs
+    #import doctest
+    #doctest.testmod()
+    main()

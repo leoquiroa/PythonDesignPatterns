bb59e632dd9a7958dac26b44056b1298ddc04022
leoquiroa <leonelquiroa@gmail.com>
2021-01-10 17:19:14 -0600

    add new characters in the game factory

--- scripts/Creational/AbstractFactory1.py
+++ scripts/Creational/AbstractFactory1.py
@@ -23,7 +23,67 @@ class FrogWorld:
     def make_obstacle(self):
         return Bug()
 
-        
+##############################################################################################
+
+class Wizard:
+    def __init__(self, name):
+        self.name = name
+    def __str__(self):
+        return self.name
+    def interact_with(self, obstacle):
+        print('{} the Wizard battles against {} and{}!'.format(self, obstacle, obstacle.action()))
+
+class Ork:
+    def __str__(self):
+        return 'an evil ork'
+    def action(self):
+        return 'kills it'
+
+class WizardWorld:
+    def __init__(self, name):
+        print(self)
+        self.player_name = name
+    def __str__(self):
+        return '\n\n\t------ Wizard World -------'
+    def make_character(self):
+        return Wizard(self.player_name)
+    def make_obstacle(self):
+        return Ork()
+
+##############################################################################################
+
+class GameEnvironment:
+    
+    def __init__(self, factory):
+        self.hero = factory.make_character()
+        self.obstacle = factory.make_obstacle()
+    
+    def play(self):
+        self.hero.interact_with(self.obstacle)
+    
+def validate_age(name):
+    try:
+        age = input('Welcome {}. How old are you? '.format(name))
+        age = int(age)
+    except ValueError as err:
+        print("Age {} is invalid, please try again...".format(age))
+        return (False, age)
+    return (True, age)
+    
+def main():
+    name = input("Hello. What's your name? ")
+    valid_input = False
+    while not valid_input:
+        valid_input, age = validate_age(name)
+    game = FrogWorld if age < 18 else WizardWorld
+    #game = FrogWorld
+    environment = GameEnvironment(game(name))
+    environment.play()
+    
+if __name__ == '__main__':
+    main()
+
+'''        
 if __name__ == '__main__':
     frogworld = FrogWorld('leo')
     frog = frogworld.make_character()
@@ -31,4 +91,4 @@ if __name__ == '__main__':
     bug = frogworld.make_obstacle()
     print('bug ',bug)
     frog.interact_with(bug)
-    
+'''

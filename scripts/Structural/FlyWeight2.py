539c0b74843dbc25255a141476857a00cd8bb3a8
leoquiroa <leonelquiroa@gmail.com>
2021-01-26 15:51:24 -0600

    example 1

---
+++ scripts/Structural/FlyWeight2.py
@@ -0,0 +1,9 @@
+'''
+*What is this pattern about?
+This pattern aims to minimise the number of objects that are needed by
+a program at run-time. A Flyweight is an object shared by multiple
+contexts, and is indistinguishable from an object that is not shared.
+The state of a Flyweight should not be affected by it's context, this
+is known as its intrinsic state. The decoupling of the objects state
+from the object's context, allows the Flyweight to be shared.
+'''

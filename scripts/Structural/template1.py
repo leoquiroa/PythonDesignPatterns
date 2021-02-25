adf46bf34371092de45bcfb4a98cc343e350d4dc
leoquiroa <leonelquiroa@gmail.com>
2021-02-25 16:27:27 -0600

    refactor

--- scripts/Structural/template1.py
+++ scripts/Structural/template1.py
@@ -1,5 +1,6 @@
 from cowpy import cow
 
+# styles
 def dots_style(msg):
     msg = msg.capitalize()
     msg = '.' * 10 + msg + '.' * 10
@@ -10,13 +11,16 @@ def admire_style(msg):
 def cow_style(msg):
     msg = cow.milk_random_cow(msg)
     return msg
+#####################################################
+def main():
+    msg = 'happy coding'
+    style_list = [dots_style, admire_style, cow_style]
+    [generate_banner(msg, s) for s in style_list]
+    
 def generate_banner(msg, style=dots_style):
     print('-- start of banner --')
     print(style(msg))
     print('-- end of banner --\n\n')
-def main():
-    msg = 'happy coding'
-    [generate_banner(msg, style) for style in (dots_style, admire_style, cow_style)]
 
 if __name__ == '__main__':
     main()
